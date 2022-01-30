734f1d1545647f9fde1fc648bcd2508dc54792a3
Muhammad Ghoffar Alfauzi <58633246+Ghoffar-A@users.noreply.github.com>
2022-01-31 03:22:04 +0700

    Update klasifikasi_nasabah.py

--- klasifikasi_nasabah.py
+++ klasifikasi_nasabah.py
@@ -1,20 +1,22 @@
+from turtle import title
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 import pickle
-
+from PIL import Image
 from sklearn import tree
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 
+image = Image.open(".\Final Decision Tree.png")
+
 # memanggil model
 loaded_model = pickle.load(
-    open('./model.sav', 'rb'))
-
+    open('.\model.sav', 'rb'))
 
 def prediksinasabah(baru):
     hasilPrediksi = loaded_model.predict(baru)
@@ -23,12 +25,20 @@ def prediksinasabah(baru):
     if (hasilPrediksi == 1):
         return'Pengajuan Pinjaman DITOLAK'
 
-st.title('Klasifikasi Pemberian Pinjaman Kepada Nasabah Menggunakan Metode Decision Tree')
+st.title('Klasifikasi Pemberian Pinjaman Kepada Nasabah Bank Menggunakan Metode Decision Tree')
 
-credit_dataset = pd.read_csv("./credit_card_approval.csv")
-if st.checkbox('Tampilkan Dataset Yang Digunakan'):
-    st.subheader('Dataset')
-    st.write(credit_dataset)
+credit_dataset = pd.read_csv(".\credit_card_approval.csv")
+st.subheader('Dataset Yang Digunakan')
+st.write(credit_dataset)
+# if st.checkbox('Tampilkan Dataset Yang Digunakan'):
+#     st.subheader('Dataset')
+#     st.write(credit_dataset)
+
+# mencaritahu proporsi unik di kolom atribut
+a = credit_dataset.TARGET.value_counts()
+# if st.checkbox('Proporsi Unik Kolom Atribut'):
+st.write('Perbandingan Class Pada Dataset')
+st.write(a)
 
 # creating instance of labelencoder
 labelencoder = LabelEncoder()
@@ -42,12 +52,12 @@ credit_dataset['NAME_EDUCATION_TYPE'] = labelencoder.fit_transform(credit_datase
 credit_dataset['NAME_FAMILY_STATUS'] = labelencoder.fit_transform(credit_dataset['NAME_FAMILY_STATUS'])
 credit_dataset['NAME_HOUSING_TYPE'] = labelencoder.fit_transform(credit_dataset['NAME_HOUSING_TYPE'])
 credit_dataset['JOB'] = labelencoder.fit_transform(credit_dataset['JOB'])
-if st.button('Ubah Huruf Menjadi Angka'):
-    st.write('Pengubahan Berhasil')
+# if st.button('Ubah Huruf Menjadi Angka'):
+#     st.write('Pengubahan Berhasil')
 
-if st.checkbox('Menampilkan Hasil Label Encoder'):
-    st.subheader('Hasil Label Encoder')
-    st.write(credit_dataset)
+# if st.checkbox('Menampilkan Hasil Label Encoder'):
+st.subheader('Hasil Label Encoder')
+st.write(credit_dataset)
 
 #feature engineering (mengambil atribut yang ingin digunakan)
 X=credit_dataset[['CODE_GENDER','FLAG_OWN_CAR','FLAG_OWN_REALTY','CNT_CHILDREN','AMT_INCOME_TOTAL','NAME_EDUCATION_TYPE',
@@ -57,47 +67,51 @@ X=credit_dataset[['CODE_GENDER','FLAG_OWN_CAR','FLAG_OWN_REALTY','CNT_CHILDREN',
 y = credit_dataset.TARGET
 # membagi dari total data menjadi 70% data Latih dan 30% data Uji
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-if st.button('Membagi Dataset Menjadi Data Latih Dan Data Uji'):
-    st.write('Pembagian Berhasil')
+#if st.button('Membagi Dataset Menjadi Data Latih Dan Data Uji'):
+    #st.write('Pembagian Berhasil')
 
-if st.checkbox('Menampilkan Data Latih dan Data Uji'):
-    st.subheader('Data Latih Fitur dan Target')
-    st.write(X_train, y_train)
-    st.subheader('Data Uji Fitur dan Target')
-    st.write(X_test, y_test)
-
-
-# mencaritahu proporsi unik di kolom atribut
-# a = credit_dataset.TARGET.value_counts()
-# if st.checkbox('Proporsi Unik Kolom Atribut'):
-#     st.write(a)
+# if st.checkbox('Menampilkan Data Latih dan Data Uji'):
+st.subheader('Membagi Data Latih dan Data Uji')
+st.write('Data Latih Fitur dan Target')
+st.write(X_train, y_train)
+st.write('Data Uji Fitur dan Target')
+st.write(X_test, y_test)
 
 #mendefinisikan model Tree
 clf = tree.DecisionTreeClassifier(criterion="entropy", splitter="best", max_depth=None, min_samples_split=2, 
                                   min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features=None, 
                                   random_state=None, max_leaf_nodes=None, min_impurity_decrease=0.0, 
                                   class_weight=None, ccp_alpha=0.0)
-if st.button('Mempersiapkan Model Tree'):
-    st.write('Persiapan Pembuatan Model Selesai')
+# if st.button('Mempersiapkan Model Tree'):
+#     st.write('Persiapan Pembuatan Model Selesai')
 
 #melakukan training model
 clf = clf.fit(X_train, y_train)
-if st.button("Pelatihan Data latih"):
-    st.write('Model Telah Didapatkan')
+# if st.button("Pelatihan Data latih"):
+#     st.write('Model Telah Didapatkan')
 
 #melakukan ploting model training
 a = tree.plot_tree(clf)
-if st.checkbox('Menampilkan Hasil Pemodelan'):
-    st.write(a)
+
+# if st.checkbox('Menampilkan Hasil Pemodelan'):
+#     st.write(a)
+
+#Menampilkan gambar
+st.subheader('Model Hasil Pelatihan')
+st.image(image)
 
 # prediksi data uji
 hasilPrediksi = clf.predict(X_test)
-if st.checkbox('Pengujian Model Menggunakan Data Uji'):
-    st.write('Pengujian Berhasil')
+# if st.checkbox('Pengujian Model Menggunakan Data Uji'):
+#     st.write('Pengujian Berhasil')
 
 #Menampilkan hasil prediksi
-if st.checkbox('Hasil Pengujian'):
+# if st.checkbox('Hasil Pengujian'):
+st.subheader('Pengujian Model Menggunakan Data Uji')
+col1, col2 = st.columns(2)
+with col1:
     st.write('Label Sebenarnya', y_test)
+with col2:
     st.write('Hasil Prediksi', hasilPrediksi)
 
 # membuat confusion matrix
@@ -109,15 +123,16 @@ cm_df = pd.DataFrame(cm,
                      columns = ['DIterima','TidakDIterima'])
 
 # Plotting the confusion matrix
-if st.button("Menampilkan Plot Yang Berkorelasit"):
-    st.set_option('deprecation.showPyplotGlobalUse', False)
-    st.write("### Heatmap")
-    fig, ax = plt.subplots(figsize=(5, 4))
-    st.write(sns.heatmap(cm_df, annot=True, fmt='d'))
-    st.pyplot()
+#if st.button("Menampilkan Plot Yang Berkorelasit"):
+st.subheader('Menampilkan Hasil Ploting Yang Berkorelasi')
+st.set_option('deprecation.showPyplotGlobalUse', False)
+st.write("Heatmap")
+fig, ax = plt.subplots(figsize=(5, 4))
+st.write(sns.heatmap(cm_df, annot=True, fmt='d'))
+st.pyplot()
 # model report
-    target = ['Diterima', 'Ditolak']
-    st.write(classification_report(y_test, hasilPrediksi, target_names=target))
+target = ['Diterima', 'Ditolak']
+st.write(classification_report(y_test, hasilPrediksi, target_names=target))
 
 def main():
     st.header(' Pengujian model dengan menggunakan data baru: ')
@@ -187,3 +202,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
